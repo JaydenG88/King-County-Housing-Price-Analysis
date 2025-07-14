6f5d6c9716b409fab1964cf620117eedd1c2800e
Jayden <Jaydenbro8868@gmail.com>
2025-07-13 20:23:19 -0700

    re-add dynamic in correlations page

--- frontend/king-county-dashboard/src/app/correlations/page.js
+++ frontend/king-county-dashboard/src/app/correlations/page.js
@@ -1,5 +1,8 @@
 import dynamic from "next/dynamic";
-import CorrelationHeatMap from "@/components/charts/CorrelationHeatMap";
+
+const CorrelationHeatMap = dynamic(() => import('@/components/charts/CorrelationHeatMap'), {
+    ssr: false,
+});
 
 export default function CorrelationsPage() {
     return (
