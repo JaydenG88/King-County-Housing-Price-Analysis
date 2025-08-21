d80d2d053e0a575b71b158ccb23be791eda1ff6d
Jayden <Jaydenbro8868@gmail.com>
2025-08-21 14:10:02 -0700

    add proxyFix to make limit apply per user

--- backend/housing_data_API/app.py
+++ backend/housing_data_API/app.py
@@ -3,14 +3,16 @@ from flask_cors import CORS
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from housing_data_API.routes.routes import housing_bp
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 limiter = Limiter(
     get_remote_address,
-    default_limits=["200 per day", "50 per hour"]
+    default_limits=["200 per day", "100 per hour"]
 )
 
 app = Flask(__name__)
 CORS(app)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
 limiter.init_app(app)
 app.register_blueprint(housing_bp, url_prefix="/api")
 
